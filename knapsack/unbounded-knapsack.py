bcf263e767e57a128dbb93a3fab284ac64f220fe
xjasonlyu <xjasonlyu@gmail.com>
2023-10-04 20:41:29 -0400

    add solutions for knapsack problems

---
+++ knapsack/unbounded-knapsack.py
@@ -0,0 +1,29 @@
+
+v = [15, 17, 14, 19, 11]
+w = [4, 5, 3, 6, 2]
+
+W = 11
+
+x, y = len(v)+1, W+1
+
+
+dp = [[0] * y for _ in range(x)]
+so = [[[]] * y for _ in range(x)]
+
+for i in range(1, x):
+    for j in range(1, y):
+        dp[i][j] = dp[i-1][j]
+        so[i][j] = so[i-1][j]
+        if j >= w[i-1]:
+            dp[i][j] = max(dp[i-1][j], v[i-1]+dp[i][j-w[i-1]])
+
+            if v[i-1]+dp[i][j-w[i-1]] >= dp[i-1][j]:
+                so[i][j] = so[i][j-w[i-1]]+[i-1]
+
+for i in dp:
+    for j in i:
+        print(f'[{j}], ', end='')
+    print()
+
+print(dp[-1][-1])
+print(so[-1][-1])
